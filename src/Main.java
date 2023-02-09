ec75a35376fe1a8203ff83833525602fe1a69a82
Roshan Kumar <roshangupta00112@gmail.com>
2023-02-09 18:43:36 +0530

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+package com.driver;
+
+
+public class Main {
+
+    public static class A{
+        public String meth(){
+            return "Invoking method from class A";
+        }
+    }
+    public static class B extends A{
+        @Override
+        public String meth(){
+           return "Method is overridden in Extended class B";
+       }
+    }
+
+
+    public static void main(String[] args) {
+        B b1=new B();
+        b1.meth();
+        b1.meth();
+    }
+
+}
